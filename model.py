6c577b34e3bb8ee70d2c8457d3b6b8344b6340ad
황상민 <sangmin@MacBook-Air-3.local>
2023-08-14 15:40:04 +0900

    Initial Commit

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class Transformer(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x):
+        return x
+    
+
+model = Transformer()
