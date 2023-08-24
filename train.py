9fcc5b612b8d1bacbc8efbd774227da6b5b92047
황상민 <sangmin@MacBook-Air-3.local>
2023-08-25 04:31:50 +0900

    feat: Implement training pipeline

---
+++ train.py
@@ -0,0 +1,31 @@
+from tqdm import tqdm
+import numpy as np
+
+from tokenizer import prepare_tokenizer
+
+def train(model, optimizer, criterion, train_loader, device, num_epochs=10):
+    model.train()
+    losses = []
+
+    for epoch in range(1, num_epochs + 1):
+        with tqdm(train_loader, total=len(train_loader), desc=f'Epoch {epoch}') as pbar:
+            for batch in pbar:
+                batch = {k: v.to(device) for k, v in batch.items()}
+
+                enc_inputs, dec_inputs, labels = batch['src'], batch['tgt'], batch['label'] # (bs, seq_len)
+                outputs = model(enc_inputs, dec_inputs) # (bs, seq_len, dec_vocab_size)
+
+                outputs = outputs.contiguous().view(-1, outputs.shape[-1]) # (bs * seq_len, dec_vocab_size)
+                labels = labels.contiguous().view(-1) # (bs * seq_len)
+
+                # print(outputs.shape, labels.shape)
+                loss = criterion(outputs, labels)
+                losses.append(loss.item())
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                pbar.set_postfix(loss=np.mean(losses))
+        
+    return losses
+
