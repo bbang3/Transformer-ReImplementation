fefb95503366cd2d8de899112b22b7740797d66c
bbang3 <hwsmin920@gmail.com>
2023-08-29 03:11:54 -0400

    feat: Add Validation process

---
+++ main.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from tokenizer import prepare_tokenizer
+from dataset import TranslationDataset
+from model.transformer import Transformer
+from train import train
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+src_tokenizer = prepare_tokenizer('./data', 'en', is_target=False)
+tgt_tokenizer = prepare_tokenizer('./data', 'de', is_target=True)
+
+train_dataset = TranslationDataset('./data', 'train', src_tokenizer, tgt_tokenizer, language_pair='en-de', max_length=256)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+val_dataset = TranslationDataset('./data', 'validation', src_tokenizer, tgt_tokenizer, language_pair='en-de', max_length=256)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+# print(train_dataset[0])
+ex = train_dataset[0]
+print(ex['src'].shape, ex['tgt'].shape)
+
+model = Transformer(device=device)
+# model = nn.DataParallel(Transformer(device=device))
+
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+criterion = torch.nn.CrossEntropyLoss(ignore_index=tgt_tokenizer.token_to_id("[PAD]"))
+
+train(model, optimizer, criterion, train_loader, val_loader, device, tgt_tokenizer, num_epochs=10)
--- train.py
+++ train.py
@@ -1,10 +1,12 @@
+import torch
 from tqdm import tqdm
 import numpy as np
 
 from tokenizer import prepare_tokenizer
 
-def train(model, optimizer, criterion, train_loader, device, tgt_tokenizer, num_epochs=10):
+def train(model, optimizer, criterion, train_loader, val_loader, device, tgt_tokenizer, num_epochs=10):
     model.train()
+    model = model.to(device)
     losses = []
 
     for epoch in range(1, num_epochs + 1):
@@ -15,8 +17,8 @@ def train(model, optimizer, criterion, train_loader, device, tgt_tokenizer, num_
                 enc_inputs, dec_inputs, labels = batch['src'], batch['tgt'], batch['label'] # (bs, seq_len)
                 outputs = model(enc_inputs, dec_inputs) # (bs, seq_len, dec_vocab_size)
 
-                decoded = tgt_tokenizer.decode(outputs[0].argmax(dim=-1).tolist())
-                print(len(decoded), decoded)
+                # decoded = tgt_tokenizer.decode(outputs[0].argmax(dim=-1).tolist())
+                # print(len(decoded), decoded)
                 outputs = outputs.contiguous().view(-1, outputs.shape[-1]) # (bs * seq_len, dec_vocab_size)
                 labels = labels.contiguous().view(-1) # (bs * seq_len)
 
@@ -27,7 +29,33 @@ def train(model, optimizer, criterion, train_loader, device, tgt_tokenizer, num_
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-                pbar.set_postfix(loss=np.mean(losses))
+                pbar.set_postfix({"loss": np.mean(losses)})
+        
+        evaluate(model, criterion, val_loader, device, tgt_tokenizer)
+        torch.save(model.state_dict(), f'./checkpoints/model_{epoch}.pt')
         
     return losses
 
+
+def evaluate(model, criterion, val_loader, device, tgt_tokenizer):
+    model.eval()
+    model = model.to(device)
+    losses = []
+
+    with torch.no_grad():
+        with tqdm(val_loader, total=len(val_loader), desc=f'Validation') as pbar:
+            for batch in val_loader:
+                batch = {k: v.to(device) for k, v in batch.items()}
+
+                enc_inputs, dec_inputs, labels = batch['src'], batch['tgt'], batch['label'] # (bs, seq_len)
+                outputs = model(enc_inputs, dec_inputs) # (bs, seq_len, dec_vocab_size)
+
+                outputs = outputs.contiguous().view(-1, outputs.shape[-1]) # (bs * seq_len, dec_vocab_size)
+                labels = labels.contiguous().view(-1) # (bs * seq_len)
+
+                loss = criterion(outputs, labels)
+                losses.append(loss.item())
+ 
+                pbar.set_postfix({"val_loss": np.mean(losses)})
+
+    return np.mean(losses)
